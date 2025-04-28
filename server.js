11675c46b827d9b15279a726c17b98139aba1f4f
Houssem <houssemnaffouti28@gmail.com>
2025-04-28 23:40:10 +0200

    Fix filename case for userController

--- server.js
+++ server.js
@@ -58,5 +58,5 @@ app.use("/notification", require("./routes/notificationrouter"));
 // Routes
 app.use('/api/admin', adminRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
